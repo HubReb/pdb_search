ab322f87af19416b1b895c3935dcd0199a6f73d4
HubReb <HubReb@users.noreply.github.com>
2023-04-02 02:04:34 +0200

    add documentation

--- paper_sorts/psycopg_db.py
+++ paper_sorts/psycopg_db.py
@@ -15,6 +15,14 @@ class PsycopgDB:
         logger_name: str = "psycopg_logger",
         log_file: str = "psycopg_logger.log",
     ):
+        """
+        Class to handle interaction with the postgresql database via the python package psycopg2.
+
+        :param config_parameters: dictionary containing the configuration that defines the database interaction
+        :param logging_level: specifies the level of the logger
+        :param logger_name: name of the logger to use
+        :param log_file: name of the file the logging information is written to
+        """
         self.config_parameters = config_parameters
         # mostly taken from https://docs.python.org/3/howto/logging.html#logging-basic-tutorial
         # create logger
@@ -36,15 +44,26 @@ class PsycopgDB:
         # add ch to logger
         self.logger.addHandler(ch)
 
-    def get_connection_and_cursor(self) -> [connection, cursor]:
+    def create_connection_and_cursor(self) -> [connection, cursor]:
+        """
+        Initialize connection to postgseql database and create cursor.
+        :return: connection to database and the cursor
+        """
         con = connect(**self.config_parameters)
         cur = con.cursor()
         return con, cur
 
     def store_in_db(self, query: str, format_arguments: Tuple[str, ...] = None) -> None:
+        """
+        Add a new entry in the database.
+
+        :param query: query to perform on the database
+        :param format_arguments: arguments to augment the query
+        :raises DatabaseError: if interaction with the database failed due to an incorrect query
+        """
         con = None
         try:
-            con, cur = self.get_connection_and_cursor()
+            con, cur = self.create_connection_and_cursor()
             if not format_arguments:
                 cur.execute(sql.SQL(query))
             else:
@@ -65,9 +84,17 @@ class PsycopgDB:
     def fetch_from_db(
         self, query: str, format_arguments: Tuple[str, ...] = None
     ) -> List:
+        """
+        Search the database for information.
+
+        :param query: query to the database
+        :param format_arguments: arguments for the query
+        :return: results extracted from the database
+        :raises DatabaseError: if interaction with the database failed due to an incorrect query
+        """
         con = None
         try:
-            con, cur = self.get_connection_and_cursor()
+            con, cur = self.create_connection_and_cursor()
             if not format_arguments:
                 cur.execute(sql.SQL(query))
             else:
@@ -84,9 +111,16 @@ class PsycopgDB:
     def delete_from_db(
         self, query: str, format_arguments: Tuple[str, ...] = None
     ) -> None:
+        """
+        Delete information from the database.
+
+        :param query: specification what to delete
+        :param format_arguments: string arguments to augment the query
+        :raises DatabaseError: if interaction with the database failed due to an incorrect query
+        """
         con = None
         try:
-            con, cur = self.get_connection_and_cursor()
+            con, cur = self.create_connection_and_cursor()
             if not format_arguments:
                 cur.execute(sql.SQL(query))
             else:
@@ -101,9 +135,16 @@ class PsycopgDB:
                 con.close()
 
     def update_db_entry(self, query: str, identifier: str, update_value: str) -> None:
+        """
+        Update information in the database.
+
+        :param query: specification what to change
+        :param identifier: identifier of the table entry to change information in
+        :param update_value: new value to set in the table
+        """
         con = None
         try:
-            con, cur = self.get_connection_and_cursor()
+            con, cur = self.create_connection_and_cursor()
             cur.execute(sql.SQL(query),  (update_value, identifier))
             con.commit()
             con.close()
@@ -112,4 +153,3 @@ class PsycopgDB:
             if con:
                 con.rollback()
                 con.close()
-        pass
